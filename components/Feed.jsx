7a6821bd619484759c8b7c5edcb691c27660003f
abdulrahman-as2104994 <as2104994@qu.edu.qa>
2024-01-08 20:29:40 +0300

    done with video
    no deployment yet and other features like search

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,56 +1,53 @@
-"use client"
-import {useState, useEffect} from 'react'
-import PromptCard from './PromptCard'
+"use client";
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
 
-const PromptCardList = ({data, handleTagClick}) => {
+const PromptCardList = ({ data, handleTagClick }) => {
   return (
-    <div className='mt-16 prompt_layout'>
-      {data.map( (post) => (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
         <PromptCard
-        key={post._id}
-        post={post}
-        handleTagClick={handleTagClick} />
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
       ))}
     </div>
-  )
-}
+  );
+};
 
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
 
-  const handleSearchChange = (e) => {
-    
-  }
+  const handleSearchChange = (e) => {};
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch('/api/prompt');
+      const response = await fetch("/api/prompt");
       const data = await response.json();
 
       setPosts(data);
-    }
+    };
     fetchPosts();
   }, []);
 
   return (
-    <section className='feed'>
+    <section className="feed">
       <form className="relative w-full flex-center">
-        <input 
-        type="text"
-        placeholder='Search for a tag or a username'
-        value={searchText}
-        onChange={handleSearchChange}
-        className='search_input peer'
-        required />
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          className="search_input peer"
+          required
+        />
       </form>
 
-      <PromptCardList
-      data={posts}
-      handleTagClick={() => {}} />
-
+      <PromptCardList data={posts} handleTagClick={() => {}} />
     </section>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
--- models/user.js
+++ models/user.js
@@ -18,4 +18,4 @@ const userSchema = new Schema({
 
 const User = models.User || model("User", userSchema);
 
-export default User;
+export default User; 
